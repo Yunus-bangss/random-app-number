a4d3fe2742316b373a3a6ada2c8af8f6089ed928
Yunus-bangss <abdoucom837@gmail.com>
2025-12-04 12:58:00 +0000

    add not todo file

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 console.log("Hey");
 const randomNumberEl = document.querySelector("#random-number")
 randomNumberEl.addEventListener("click" , () =>{
-    document.querySelector("#result").textContent = Math.random()
+    document.querySelector("#result").textContent = Math.floor(
+        getRandomNumber(1,100)
+    )
 })
+function getRandomNumber (min, max){
+    return Math.random() * (max - min ) + min 
+}
