5a55a384ad3b929051b904bacab66b844bbab1ca
Yunus-bangss <abdoucom837@gmail.com>
2025-12-04 13:19:25 +0000

    delete console

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hey");
+
 const randomNumberEl = document.querySelector("#random-number")
 randomNumberEl.addEventListener("click" , () =>{
     document.querySelector("#result").textContent = Math.floor(
