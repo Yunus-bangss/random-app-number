af6f1fd2781816d8bff17f19b97fcd6e63188477
Yunus-bangss <abdoucom837@gmail.com>
2025-12-04 12:51:58 +0000

    feature ad random button

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Hey");
+const randomNumberEl = document.querySelector("#random-number")
+randomNumberEl.addEventListener("click" , () =>{
+    document.querySelector("#result").textContent = Math.random()
+})
